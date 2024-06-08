5a020c4f201c9f9ebfeeb08da358d36252d6d7e3
jesicabral <jesicabral@hotmail.com>
2024-06-08 01:42:13 -0300

    creamos metodo deleteProductInCart

--- src/dao/mongoDao/cart.dao.js
+++ src/dao/mongoDao/cart.dao.js
@@ -17,12 +17,12 @@ const addProductToCart = async (cid, pid) => {
     product: false
   }
 
-  const productInCart = await cartModel.findByIdAndUpdate({_id: cid, "products.product": pid}, {$inc: { "products.$.quantity": 1}});
+  const productInCart = await cartModel.findOneAndReplace({_id: cid, "products.product": pid}, {$inc: { "products.$.quantity": 1}});
   if(!productInCart) {
-    await cartModel.findByIdAndUpdate(cid, {$push: {products: {product: pid, quantity: 1 } } });
+    await cartModel.findOneAndUpdate(cid, {$push: {products: {product: pid, quantity: 1 } } });
   };
      
-  const cart = await cartModel.findByIdAndUpdate(cid);
+  const cart = await cartModel.findById(cid);
 
   return cart;
 
